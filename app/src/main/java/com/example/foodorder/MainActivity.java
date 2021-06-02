23aa2f81856a1cf45ae15946f6cac9822f77361e
Zhuldyz20 <zhuldyz.ilyassova2001@gmail.com>
2021-06-02 14:08:26 +0600

    Added methods to get restaurant data and RestaurantListAdapter class

--- app/src/main/java/com/example/foodorder/MainActivity.java
+++ app/src/main/java/com/example/foodorder/MainActivity.java
@@ -2,8 +2,22 @@ package com.example.foodorder;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.foodorder.model.RestaurantModel;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +28,38 @@ public class MainActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Restaurant List");
+        List <RestaurantModel> restaurantModelList = getRestaurantData();
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView rv = findViewById(R.id.recyclerView);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+        rv.setAdapter();
+
+    }
+
+    private List <RestaurantModel> getRestaurantData() {
+       InputStream is = getResources().openRawResource(R.raw.restaurants);
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            int n;
+            while(( n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0,n);
+            }
+
+        }catch (Exception e) {
+
+        }
+
+        String jsonStr = writer.toString();
+        Gson gson = new Gson();
+        RestaurantModel[] restaurantModels = gson.fromJson(jsonStr, RestaurantModel[].class);
+        List<RestaurantModel> restList = Arrays.asList(restaurantModels);
+
+        return restList;
     }
 }
