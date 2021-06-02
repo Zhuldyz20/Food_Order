0bd291a800eac1b02005b535d07514c9602f03db
Zhuldyz20 <zhuldyz.ilyassova2001@gmail.com>
2021-06-02 19:44:05 +0600

    Created Restaurants Hours class

--- app/src/main/java/com/example/foodorder/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/example/foodorder/adapters/RestaurantListAdapter.java
@@ -1,24 +1,60 @@
 package com.example.foodorder.adapters;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RestaurantListAdapter extends RecyclerView.Adapter<> {
+import com.example.foodorder.R;
+import com.example.foodorder.model.RestaurantModel;
+
+import java.util.List;
+
+public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.MyViewHolder> {
+
+    private List<RestaurantModel> restaurantModelList;
+    public RestaurantListAdapter(List<RestaurantModel> restaurantModelList){
+        this.restaurantModelList = restaurantModelList;
+    }
+
+    public void updateData(List<RestaurantModel> restaurantModelList){
+        this.restaurantModelList = restaurantModelList;
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public RestaurantListAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_row, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RestaurantListAdapter.MyViewHolder holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return restaurantModelList.size();
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView restaurantName;
+        TextView restaurantAddress;
+        TextView restaurantHours;
+        ImageView thumbImage;
+
+        public MyViewHolder(View view){
+             super(view);
+            restaurantName = view.findViewById(R.id.restaurantName);
+            restaurantAddress = view.findViewById(R.id.restaurantAddress);
+            restaurantHours = view.findViewById(R.id.restaurantHours);
+            thumbImage = view.findViewById(R.id.thumbImage);
+        }
     }
 }
--- app/src/main/java/com/example/foodorder/model/RestaurantModel.java
+++ app/src/main/java/com/example/foodorder/model/RestaurantModel.java
@@ -1,4 +1,10 @@
 package com.example.foodorder.model;
 
 public class RestaurantModel {
+
+    private String name;
+    private String address;
+    private String image;
+    private float delivery_charge;
+
 }
