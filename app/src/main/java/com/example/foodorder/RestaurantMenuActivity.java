cfc73566ed3c474df544d4f625e55ecd799a60a6
Zhuldyz20 <zhuldyz.ilyassova2001@gmail.com>
2021-06-16 17:53:36 +0600

    Created MenuList adapter

--- app/src/main/java/com/example/foodorder/RestaurantMenuActivity.java
+++ app/src/main/java/com/example/foodorder/RestaurantMenuActivity.java
@@ -1,14 +1,50 @@
 package com.example.foodorder;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.foodorder.model.Menus;
+import com.example.foodorder.model.RestaurantModel;
+
+import java.util.List;
 
 public class RestaurantMenuActivity extends AppCompatActivity {
 
+    private List<Menus> menuList = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_menu);
+
+        RestaurantModel restaurantModel = getIntent().getParcelableExtra("RestaurantModel");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(restaurantModel.getName());
+        actionBar.setSubtitle(restaurantModel.getAddress());
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        menuList = restaurantModel.getMenus();
+        initRecyclerView();
+
+        TextView buttonCheckout = findViewById(R.id.buttonCheckout);
+        buttonCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        recyclerView.setAdapter();
     }
 }
